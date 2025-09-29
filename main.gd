712f010a48e3083d22b0e3b0ab5251ac90accad9
Julien <juliengkz@gmail.com>
2025-09-29 17:26:07 +0200

    Première update, on a fait le hitmarker et vu les tween

--- main.gd
+++ main.gd
@@ -6,10 +6,13 @@ var upgrade_1_cost: int = 10
 var upgrade_1_shown: bool = false
 
 
+
 @onready var click_button: Button = $CenterContainer/VBoxContainer/ClickButton
 @onready var score_label: Label = $CenterContainer/VBoxContainer/ScoreLabel
 @onready var upgrade_button_1: Button = $HBoxContainer/UpgradeButton1
 @onready var upgrade_label_1: Label = $HBoxContainer/UpgradeLabel1
+@onready var hitmarker_template = $HitMarker
+
 
 
 func _ready() -> void:
@@ -22,6 +25,26 @@ func _ready() -> void:
 
 func _process(_delta):
 	upgrade_button_1.disabled = score < upgrade_1_cost
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		_spawn_hitmarker()
+		
+
+func _spawn_hitmarker():
+	var marker = hitmarker_template.duplicate()
+	add_child(marker)
+	marker.visible = true
+	marker.position = get_global_mouse_position()
+	marker.modulate.a = 1.0
+	marker.scale = Vector2(0.035, 0.035)
+	
+	var tween = create_tween()
+	tween.tween_property(marker, "scale", Vector2(0.05, 0.05), 0.05).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	tween.tween_property(marker, "scale", Vector2(0.035, 0.035), 0.05).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.parallel().tween_property(marker, "modulate:a", 0.0, 0.1)
+	tween.connect("finished", Callable(marker, "queue_free"))
 	
 
 func _on_UpgradeButton1_pressed() -> void:
